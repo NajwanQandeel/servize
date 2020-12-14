1ba149b24e0c67c5a7172129f43a362742891025
NajwanQandeel <najwan.qandeel@gmail.com>
2020-12-14 10:43:56 +0200

    merge with main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,46 +24,27 @@ function App() {
       <div className="App">
 
         <Navbar />
-<<<<<<< HEAD
         <Signup />
         <Login />
-        {/* <ProviderSignup />  */}
-         <Catagories />
-
-        <Switch>
-
-          <Route exact path="/" component={Main} />
-          <Route exact path="/" component={Categories} />
-          {/* <Route path="/prov/signup" component={ProviderSignup} /> */}
-          <Route path="/user/signup" component={Signup} />
-          <Route path="/user/login" component={Login} />
-          {/* <Route path="/provider" component={ProviderContainer} /> */}
-          <Route path="/reset_password" component={ForgotPassword} />
-          <Route path="/reset_password" component={ResetPassword} />
-          <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
-=======
-        {/* <Signup />
-        <Login /> */}
         {/* <ProviderSignup /> */}
-        {/* <Catagories /> */}
-        {/* <Search /> */}
+        <Catagories />
+        <Search />
 
         <Switch>
 
-          {/* <Route exact path="/" component={Main} /> */}
-          {/* <Route exact path="/" component={Categories} /> */}
-          {/* <Route path="/prov/signup" component={ProviderSignup} /> */}
-          {/* <Route path="/user/signup" component={Signup} /> */}
-          {/* <Route path="/user/login" component={Login} /> */}
-          {/* <Route path="/provider" component={ProviderContainer} /> */}
+          <Route exact path="/" component={Main} /> 
+          <Route exact path="/" component={Categories} /> 
+          {/* <Route path="/prov/signup" component={ProviderSignup} />  */}
+         <Route path="/user/signup" component={Signup} />
+           <Route path="/user/login" component={Login} /> 
+         {/* <Route path="/provider" component={ProviderContainer} />
           <Route path="/auth/users/reset_password/" component={ForgotPassword} />
           <Route path="/reset/:token" component={ResetPassword} />
->>>>>>> 57236b1aeb8935a1aef9e126eaed1630b81ff683
-          <Route path="/activate/:uid/:token" component={Activate} />
+          <Route path="/activate/:uid/:token" component={Activate} /> */}
 
         </Switch>
 
-        <Footer />
+        <Footer/>
 
       </div>
     </Router>
--- client/src/store.tsx
+++ client/src/store.tsx
@@ -3,24 +3,17 @@ import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
 import persistedReducer from './reducers/rootReducer';
 
-// const initialState = {};
+const initialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-<<<<<<< HEAD
-    persistedReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-=======
-    persistedReducer
+    // persistedReducer,
     // initialState,
     // compose(
-        // applyMiddleware(...middleware),
->>>>>>> 57236b1aeb8935a1aef9e126eaed1630b81ff683
+    //     applyMiddleware(...middleware),
         // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    // )
 );
 
 const persistor = persistStore(store);
